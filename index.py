650dd0c768e8b1aa0bfe7b3f0124f2c4fa01fcf8
omaranBazna <omran4d@gmail.com>
2025-01-15 23:22:07 -0500

    c

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import os
 import json
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 
 port = os.getenv("PORT", 8000)
